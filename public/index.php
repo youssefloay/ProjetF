a0e0d3e989a011159266a673944a06488ac4b1c9
Lucas <lucas@ld-web.net>
2021-10-07 12:55:09 +0200

    User insert + img assets request filter

--- public/index.php
+++ public/index.php
@@ -3,6 +3,14 @@
 // Inclut l'autoloader généré par Composer
 require_once __DIR__ . "/../vendor/autoload.php";
 
+if (
+  php_sapi_name() !== 'cli' &&
+  preg_match('/\.(?:png|jpg|jpeg|gif|ico)$/', $_SERVER['REQUEST_URI'])
+) {
+  return false;
+}
+
+use App\Entity\User;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
@@ -29,3 +37,17 @@ $config = Setup::createAnnotationMetadataConfiguration(
 
 // Un gestionnaire d'entités = paramètres de connexion + objet configuration
 $entityManager = EntityManager::create($dbParams, $config);
+
+$user = new User();
+
+$user->setName("Bob")
+  ->setFirstName("John")
+  ->setUsername("Bobby")
+  ->setPassword("randompass")
+  ->setEmail("bob@bob.com")
+  ->setBirthDate(new DateTime('1981-02-16'));
+
+// On demande au gestionnaire d'entités de persister l'objet
+// Attention, à ce moment-là l'objet n'est pas encore enregistré en BDD
+$entityManager->persist($user);
+$entityManager->flush();
