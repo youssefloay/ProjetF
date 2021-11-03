3857532079ba3091bec84c1734ffc1386f26b433
Lucas <lucas@ld-web.net>
2021-11-03 16:12:41 +0100

    Refactored DB Connection

--- public/index.php
+++ public/index.php
@@ -10,38 +10,20 @@ if (
   return false;
 }
 
+use App\Config\Connection;
 use App\Controller\IndexController;
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
 use Symfony\Component\Dotenv\Dotenv;
 
+// Env vars - Possibilité d'utiliser le pattern Adapter
+// Pour pouvoir varier les dépendances qu'on utilise
 $dotenv = new Dotenv();
 $dotenv->loadEnv(__DIR__ . '/../.env');
 
-// On indique à Doctrine où aller chercher les entités
-// pour les analyser et les mapper dans la base de données
-$paths = [__DIR__ . "/../src/Entity"];
-$isDevMode = ($_ENV['APP_ENV'] === 'dev');
+// BDD
+$connection = new Connection();
+$entityManager = $connection->init();
 
-$dbParams = [
-  'driver'   => $_ENV['DB_DRIVER'],
-  'host'     => $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT'],
-  'user'     => $_ENV['DB_USER'],
-  'password' => $_ENV['DB_PASSWORD'],
-  'dbname'   => $_ENV['DB_NAME'],
-];
-
-$config = Setup::createAnnotationMetadataConfiguration(
-  $paths,
-  $isDevMode,
-  null,
-  null,
-  false
-);
-
-// Un gestionnaire d'entités = paramètres de connexion + objet configuration
-$entityManager = EntityManager::create($dbParams, $config);
-
+// Routage
 $requestUri = $_SERVER['REQUEST_URI'];
 
 if ($requestUri === '/') {
