7efcb28bc43224ea78ee37f9ae9bf51fa09c7845
Lucas <lucas@ld-web.net>
2021-11-04 15:09:22 +0100

    Added debug based on env var in Twig environment

--- src/Config/TwigEnvironment.php
+++ src/Config/TwigEnvironment.php
@@ -11,6 +11,7 @@ class TwigEnvironment
   {
     $loader = new FilesystemLoader(__DIR__ . '/../../templates');
     return new Environment($loader, [
+      'debug' => $_ENV['APP_ENV'] === 'dev',
       'cache' => __DIR__ . '/../../var/cache',
     ]);
   }
